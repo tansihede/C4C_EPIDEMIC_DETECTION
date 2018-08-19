3d7b4d96136b0625f7982e31b31cb829cd1a1f07
tansihede <tansihede@gmail.com>
2018-08-19 13:47:24 +0530

    Add files via upload

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+
+//var userController = require('../controllers/userController');
+
+// server side apis here
+
+
+
+exports.dummy = function(req, res){
+   
+ // res.send("respond with a resource");
+    
+};
+    
+ 
+
+
+
+
