e0bd36cce915194dd1a8d87583f6023a215f4256
marwin menezes <marwin.menezes@gmail.com>
2018-08-23 20:38:57 +0530

    Added changes which got missed from tansi and vardan

--- javascripts/app.js
+++ javascripts/app.js
@@ -14,18 +14,36 @@ myAppModule.config(function($routeProvider,$locationProvider) {
             templateUrl: 'views/diseases/outbreak.html',
             controller: 'diseasesController'
         }).when('/', {                                        
-            templateUrl: 'views/dashboard/dashboard.html',
-            controller: 'dashboardController'
+            templateUrl: 'views/login/signin.html',
+            controller: 'userController'
         }).when('/dashboard', {
             templateUrl: 'views/dashboard/dashboard.html',
             controller: 'dashboardController'
+        }).when('/hospital', {
+            templateUrl: 'views/hospital/hospitalDashboard.html',
+            controller: 'hospitalDashboardController'
+        }).when('/patientDetails', {
+            templateUrl: 'views/hospital/patientDetails.html',
+            controller: 'userController'                    // correct controller to be changed
+        }).when('/symptomAnalysis', {
+            templateUrl: 'views/hospital/symptomAnalysis.html',
+            controller: 'userController'                    // correct controller to be changed
         }).
     otherwise({redirectTo:'/'})
 
+    
 $locationProvider.html5Mode({
   enabled: true,
   requireBase: false
 }); 
   
+});
 
+
+myAppModule.run(function($rootScope, $location){
+  $rootScope.$on('routeChangeSuccess', function(event, next, current){
+    if ($location.path() == '/login') {
+      $rootscope.isLoggedinHospital =  false;
+    };
+  });
 });
