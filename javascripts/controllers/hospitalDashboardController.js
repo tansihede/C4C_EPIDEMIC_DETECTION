81d4db35aab9510cef3d86a7f30e005b321231dc
Vardan Nadkarni <vardan_nadkarni@persistent.co.in>
2018-09-21 17:55:19 +0530

    Adds Query Blockchain API to Hospital Dashboard

--- javascripts/controllers/hospitalDashboardController.js
+++ javascripts/controllers/hospitalDashboardController.js
@@ -1,10 +1,10 @@
-angular.module('myApp').controller('hospitalDashboardController', ['$scope', '$http','$compile', 'DTOptionsBuilder', 'DTColumnBuilder', function ($scope, $http, $compile, DTOptionsBuilder, DTColumnBuilder) {
+angular.module('myApp').controller('hospitalDashboardController', ['$scope', '$http', '$compile', 'DTOptionsBuilder', 'DTColumnBuilder', function ($scope, $http, $compile, DTOptionsBuilder, DTColumnBuilder) {
 
-	$scope.loaded = true;
+    $scope.loaded = true;
 
     $scope.getpatientDetails = function () {
-     
-    	$http.get('/api/getPatients').success(function (data) {
+
+        $http.get('/api/getPatients').success(function (data) {
             $scope.result = data.result;
             console.log(JSON.stringify(data.result[0]));
 
@@ -29,26 +29,42 @@ angular.module('myApp').controller('hospitalDashboardController', ['$scope', '$h
 
 
             $scope.dtColumns = dtColumns //headers
-           
+
             //create options
             $scope.dtOptions = DTOptionsBuilder.newOptions()
                 .withOption('data', dataResults)
                 .withPaginationType('full_numbers')
                 .withDisplayLength('5')
-                .withOption('lengthMenu',[5,10,15])                   
-            
+                .withOption('lengthMenu', [5, 10, 15])
+
             //initialize the dataTable
             angular.element('#example').attr('datatable', '')
             $compile(angular.element('#example'))($scope)
-           
+
             $scope.loaded = false;
-            
+
         }).error(function (data) {
             console.log('Error: ' + data);
         });
     };
-    
-    $scope.getpatientDetails();
+
+    $scope.queryBlockchain = function () {
+        $http({
+            method: "GET",
+            url: "http://10.53.18.86:4000/channels/mychannel/chaincodes/mycc?peer=peer0.org1.example.com&fcn=query&args=%5B%27%27%2C%27%27%5D",
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Mzc1Njc2ODYsInVzZXJuYW1lIjoiSmltMSIsIm9yZ05hbWUiOiJPcmcxIiwiaWF0IjoxNTM3NTMxNjg2fQ.XrscjHVFpV-nZdCvLnP5YF0IRm3eXOtz31X_mtxqlvA',
+                'content-type': 'application/json'
+            }
+        }).success(function (response) {
+            console.log(response);
+        }).error(function (error) {
+            console.log(error);
+        });
+    };
+
+    // $scope.getpatientDetails();
+    $scope.queryBlockchain();
 
 }]);
 
