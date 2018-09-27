51d9773d65084fa24ac4f338d1676829a8dd54ff
Vardan Nadkarni <vardan_nadkarni@persistent.co.in>
2018-09-27 12:22:40 +0530

    NGO Supply chain approval

--- javascripts/controllers/dashboardController.js
+++ javascripts/controllers/dashboardController.js
@@ -23,7 +23,57 @@ angular.module('myApp').controller('dashboardController', ['$scope','$window','$
       });
   };
   $scope.queryBlockchain();
-   
+
+    /** Function to Approve supply chain **/
+ 	$scope.approveRequest = function(requestId) {
+        var token = $window.localStorage.getItem('token');
+        var args = [requestId];
+        var peers =["peer0.org1.example.com"];
+        var data={
+            "peers": peers,
+            "fcn":"volunteerInvoke",
+            "args":args
+        }
+
+        $http({
+            method: "POST",
+            withCredentials : false , 
+            url: "http://10.53.18.86:4000/channels/mychannel/chaincodes/mycc",
+            headers: {
+                'Authorization': 'Bearer '+ token,
+                'content-type': 'application/json'
+            },
+            data:JSON.stringify(data)
+        }).success(function (response) {
+           $scope.queryBlockchain();
+            console.log("Approved")
+        }).error(function (error) {
+             console.log("Approve Failed")
+        });
+	 
+ 	}  // Approve supply chain
+
+    $scope.supplyChainPopup = function(hospitalSuccess, ngoSuccess, volunteerSuccess, assetRequested){
+        
+        if(hospitalSuccess){
+             document.getElementById("hospitalSuccess").setAttribute("class", "li complete");
+        }
+        if(ngoSuccess){
+            document.getElementById("ngoSuccess").setAttribute("class", "li complete");
+        }
+        if(volunteerSuccess){
+             document.getElementById("volunteerSuccess").setAttribute("class", "li complete");
+        }
+        if(assetRequested){
+            document.getElementById("assetRequested").setAttribute("class", "li complete");
+        }
+    }
+
+
+
+
+
+
 }]);
 
 
--- javascripts/controllers/diseasesController.js
+++ javascripts/controllers/diseasesController.js
@@ -1,4 +1,4 @@
-angular.module('myApp').controller('diseasesController', ['$scope', '$http','$compile','DTOptionsBuilder', 'DTColumnBuilder', function ($scope, $http,$compile, DTOptionsBuilder, DTColumnBuilder) { 
+angular.module('myApp').controller('diseasesController', ['$scope', '$window','$http','$compile','DTOptionsBuilder', 'DTColumnBuilder', function ($scope,$window, $http,$compile, DTOptionsBuilder, DTColumnBuilder) { 
 	
     $scope.loaded = true ; 
     $scope.dtInstance = {};
@@ -72,6 +72,75 @@ angular.module('myApp').controller('diseasesController', ['$scope', '$http','$co
           {text: "muscle pain", weight: 2},
 		  // ...as many words as you want
       ];
+
+
+  /*  alert("fine");*/
+    $scope.userlabel = "John";
+    $scope.requests=[];
+    $scope.queryBlockchain = function () {
+      var token = $window.localStorage.getItem('token');
+      $http({
+          method: "GET",
+          url: "http://10.53.18.86:4000/channels/mychannel/chaincodes/mycc?peer=peer0.org2.example.com&fcn=query&args=%5B%27%27%2C%27%27%5D",
+          withCredentials : false , 
+          headers: {
+              'Authorization': 'Bearer '+token,
+              'content-type': 'application/json'
+          }
+      }).success(function (response) {
+         $scope.requests =response.responce.reverse();
+         console.log(response);
+      }).error(function (error) {
+          console.log(error);
+      });
+  };
+  $scope.queryBlockchain();
+
+    /** Function to Approve supply chain **/
+ 	$scope.approveRequest = function(requestId) {
+        var token = $window.localStorage.getItem('token');
+        var args = [requestId];
+        var peers =["peer0.org1.example.com"];
+        var data={
+            "peers": peers,
+            "fcn":"volunteerInvoke",
+            "args":args
+        }
+
+        $http({
+            method: "POST",
+            withCredentials : false , 
+            url: "http://10.53.18.86:4000/channels/mychannel/chaincodes/mycc",
+            headers: {
+                'Authorization': 'Bearer '+ token,
+                'content-type': 'application/json'
+            },
+            data:JSON.stringify(data)
+        }).success(function (response) {
+           $scope.queryBlockchain();
+            console.log("Approved")
+        }).error(function (error) {
+             console.log("Approve Failed")
+        });
+	 
+ 	}  // Approve supply chain
+
+    $scope.supplyChainPopup = function(hospitalSuccess, ngoSuccess, volunteerSuccess, assetRequested){
+        
+        if(hospitalSuccess){
+             document.getElementById("hospitalSuccess").setAttribute("class", "li complete");
+        }
+        if(ngoSuccess){
+            document.getElementById("ngoSuccess").setAttribute("class", "li complete");
+        }
+        if(volunteerSuccess){
+             document.getElementById("volunteerSuccess").setAttribute("class", "li complete");
+        }
+        if(assetRequested){
+            document.getElementById("assetRequested").setAttribute("class", "li complete");
+        }
+    }
+
 
   
 }]);
